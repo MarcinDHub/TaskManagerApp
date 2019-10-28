eb21b65a70f9418f8ce5cf1ca60e90e5a0132aef
Marcin <marcindenga@gmail.com>
2019-10-28 22:30:00 +0100

    Dodaj pliki projektów.

---
+++ TaskManagerApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows;
+using Tray;
+
+namespace TaskManagerApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TrayClass trayClass;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += OnClosingWindow;
+            trayClass = new TrayClass(this);
+        }
+
+        private void OnClosingWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            trayClass.ShowTrayInformation("Tytuł", "Treść");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+
+    }
+}
---
+++ TaskManagerApp/TrayClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using TaskManagerApp;
+
+namespace Tray
+{
+    public class TrayClass
+    {
+        private MainWindow mainWindow;
+
+        private ContextMenu m_menu;
+        private NotifyIcon ni;
+
+        public TrayClass(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            /* */
+            ni = new NotifyIcon();
+            Stream iconStream = System.Windows.Application.GetResourceStream(new Uri("pack://application:,,,/Resources/ico.ico")).Stream;
+            ni.Icon = new System.Drawing.Icon(iconStream);
+            ni.Text = "Tray test";
+            ni.Visible = true;
+
+            ni.DoubleClick += TrayOpen_Click;
+
+            /* menu */
+            m_menu = new ContextMenu();
+            m_menu.MenuItems.Add(0,
+                new MenuItem("Otwórz", new System.EventHandler(TrayOpen_Click)));
+            m_menu.MenuItems.Add(1,
+                new MenuItem("Zamknij", new System.EventHandler(TrayExit_Click)));
+            ni.ContextMenu = m_menu;
+        }
+
+        public void ShowTrayInformation(string Title, string Content)
+        {
+            ni.BalloonTipTitle = Title;
+            ni.BalloonTipText = Content;
+            ni.BalloonTipIcon = ToolTipIcon.None;
+            ni.Visible = true;
+            ni.ShowBalloonTip(30000);
+
+            ni.BalloonTipClicked += delegate (object sender, EventArgs args)
+            {
+                mainWindow.Show();
+                mainWindow.Activate();
+            };
+        }
+
+        private void TrayExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TrayOpen_Click(object sender, EventArgs e)
+        {
+            mainWindow.Show();
+            mainWindow.Activate();
+        }
+    }
+}
